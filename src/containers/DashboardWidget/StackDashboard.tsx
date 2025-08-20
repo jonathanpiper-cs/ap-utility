912c143739c7bc3518ec3e6214536dc0105d289a
Jonathan Piper <jonathan.piper@contentstack.com>
2025-08-20 14:19:28 -0700

    table

--- src/containers/DashboardWidget/StackDashboard.tsx
+++ src/containers/DashboardWidget/StackDashboard.tsx
@@ -1,27 +1,25 @@
-import { useState, useEffect, useRef, useContext } from "react"
+import { useState, useEffect, useRef } from "react"
 import { InfiniteScrollTable, Icon } from "@contentstack/venus-components"
 import '@contentstack/venus-components/build/main.css'
 import { useAppSdk } from "../../common/hooks/useAppSdk"
 import "../index.css"
 import "./StackDashboard.css"
-import { MarketplaceAppContext } from "../../common/contexts/marketplaceContext"
 
 type ContentTypesResponse = { content_types?: any[] }
 
 const StackDashboardExtension = () => {
-    const hi = useContext(MarketplaceAppContext)
-    console.log(hi)
     const columns = [
         { Header: 'Content Type', accessor: 'title' },
         {
             Header: 'Entries', accessor: (data: { count: number, uid: string }) => {
-                return (data.count > 0 ?<a href={`https://app.contentstack.com/#!/stack/${stack?._data.api_key}/entries?branch=main&page=1&page_size=100&popular_view=entries-only-base&query={"queryObject":{"$and":[{"_variants":{"$in":["$baseVariant"]}}],"_content_type_uid":{"$in":["${data.uid}"]}}}`}>{data.count} - View <Icon icon="InternalLink"></Icon></a> : <>{data.count}</>)
-            }
+                return (data.count > 0 ? <a href={`https://app.contentstack.com/#!/stack/${stack?._data.api_key}/entries?branch=main&page=1&page_size=100&popular_view=entries-only-base&query={"queryObject":{"$and":[{"_variants":{"$in":["$baseVariant"]}}],"_content_type_uid":{"$in":["${data.uid}"]}}}`}>{data.count} - View <Icon icon="InternalLink"></Icon></a> : <>{data.count}</>)
+            },
+            id: 'count'
         },
         {
             Header: 'Content Type Builder', accessor: (data: { uid: any }) => {
                 return (<a href={`https://app.contentstack.com/#/stack/${stack?._data.api_key}/content-type/${data.uid}/content-type-builder?branch=main`} target="_blank">Open <Icon icon="InternalLink"></Icon></a>)
-            }
+            }, disabledSortBy: true
         },
     ]
 
@@ -30,9 +28,14 @@ const StackDashboardExtension = () => {
 
     const [contentTypes, setContentTypes] = useState<any[]>([])
     const [itemStatusMap, setItemStatusMap] = useState<string[]>([])
+    const [sortByV2, setSortByV2]: any = useState({
+        sortingDirection: 'asc',
+        id: 'title'
+    })
     const tableRef = useRef<HTMLDivElement | null>(null)
 
-    const fetchData = async () => {
+    const fetchData = async ({ sortBy, searchText, skip, limit, startIndex, stopIndex, fetchCalledByTable }: any) => {
+        console.log(sortBy, searchText, skip, limit, startIndex, stopIndex, fetchCalledByTable)
         try {
             let itemStatusMapCopy = { ...itemStatusMap }
 
@@ -40,7 +43,6 @@ const StackDashboardExtension = () => {
             let contentTypesWithCount: ContentTypesResponse = {}
             if (stack && typeof stack.getContentTypes === "function") {
                 contentTypesObj = await stack.getContentTypes()
-
                 setContentTypes(contentTypesObj.content_types ?? [])
                 if (contentTypesObj.content_types) {
                     for (let index = 0; index < contentTypesObj.content_types.length; index++) {
@@ -50,7 +52,6 @@ const StackDashboardExtension = () => {
                 setItemStatusMap(itemStatusMapCopy)
             }
             if (stack && contentTypesObj.content_types) {
-                console.log('hi')
                 contentTypesWithCount.content_types = await Promise.all(contentTypesObj.content_types.map(async (contentType) => {
                     const count = await stack.ContentType(contentType.uid).Entry.Query().count()
                     return { ...contentType, count: count.entries }
@@ -61,7 +62,18 @@ const StackDashboardExtension = () => {
                     }
                 }
                 setItemStatusMap(itemStatusMapCopy)
-                console.log("Content Types with Count:", contentTypesWithCount)
+                if (contentTypesWithCount?.content_types) {
+                    contentTypesWithCount.content_types.sort((a: any, b: any) => {
+                        if (sortBy.id === 'title') {
+                            return a[sortBy.id].localeCompare(b[sortBy.id]) * (sortBy?.sortingDirection === 'asc' ? 1 : -1)
+                        } else {
+                            console.log('a', a[sortBy.id], 'b', b[sortBy.id], a[sortBy.id] - b[sortBy.id], (sortBy?.sortingDirection === 'asc' ? 1 : -1))
+                            console.log(a[sortBy.id] - b[sortBy.id] * (sortBy?.sortingDirection === 'asc' ? 1 : -1))
+                            return (a[sortBy.id] - b[sortBy.id]) * (sortBy?.sortingDirection === 'asc' ? 1 : -1)
+                        }
+                        return 0
+                    })
+                }
                 setContentTypes(contentTypesWithCount.content_types ?? [])
             }
         } catch (error) {
@@ -70,7 +82,7 @@ const StackDashboardExtension = () => {
     }
 
     useEffect(() => {
-        fetchData()
+        fetchData({ sortBy: sortByV2, searchText: '', skip: 0, limit: 100, startIndex: 0, stopIndex: 100, fetchCalledByTable: false })
     }, [])
 
     return (
@@ -94,7 +106,7 @@ const StackDashboardExtension = () => {
                         // canSearch={true}
                         canRefresh={true}
                         ref={tableRef}
-                    // initialSortByV2={sortByV2}
+                        initialSortByV2={sortByV2}
                     // callBackForSortByV2={(sortBy) => {
                     //     console.log('sorting here callback', sortBy)
                     //     setSortByV2(sortBy)
