a85eacc658fa1a88c2dcbeaff173e55bf78b26e9
Jonathan Piper <jonathan.piper@contentstack.com>
2025-08-20 11:32:49 -0700

    init

--- src/containers/FullPage/FullPage.tsx
+++ src/containers/FullPage/FullPage.tsx
@@ -1,63 +1,109 @@
-import { useCallback, useState } from "react";
-import localeTexts from "../../common/locales/en-us/index";
-import parse from "html-react-parser";
-import { useAppConfig } from "../../common/hooks/useAppConfig";
-import "../index.css";
-import "./FullPage.css";
-import Icon from "../../assets/Full-Page-Logo.svg";
-import ReadOnly from "../../assets/lock.svg";
-import JsonView from "../../assets/JsonView.svg";
-import ConfigModal from "../../components/ConfigModal/ConfigModal";
+import { useState, useEffect, useRef, useContext } from "react"
+import { InfiniteScrollTable, Icon } from "@contentstack/venus-components"
+import '@contentstack/venus-components/build/main.css'
+import { useAppSdk } from "../../common/hooks/useAppSdk"
+import "../index.css"
+import "./FullPage.css"
+import { MarketplaceAppContext } from "../../common/contexts/marketplaceContext"
+
+type ContentTypesResponse = { content_types?: any[] }
 
 const FullPageExtension = () => {
-  const appConfig = useAppConfig();
+    const hi = useContext(MarketplaceAppContext)
+    console.log(hi)
+    const columns = [
+        { Header: 'Content Type', accessor: 'title' },
+        {
+            Header: 'Entries', accessor: (data: { count: number, uid: string }) => {
+                // return (<Pill items={[{ id: 1, text: data.count, trailingIcon: data.count !== 0 ? <Icon icon="InternalLink" /> : undefined }]} status={data.count === 0 ? 'danger' : 'default'} onPillClickHandler={() => openEntriesView(stack?._data.api_key || '', data.uid || '', data.count || 0)} shouldHaveBorder={false} variant='chip' />)
+                return (data.count > 0 ?<a href={`https://app.contentstack.com/#!/stack/${stack?._data.api_key}/entries?branch=main&page=1&page_size=100&popular_view=entries-only-base&query={"queryObject":{"$and":[{"_variants":{"$in":["$baseVariant"]}}],"_content_type_uid":{"$in":["${data.uid}"]}}}`}>{data.count} - View <Icon icon="InternalLink"></Icon></a> : <>{data.count}</>)
+            }
+        },
+        {
+            Header: 'Content Type Builder', accessor: (data: { uid: any }) => {
+                return (<a href={`https://app.contentstack.com/#/stack/${stack?._data.api_key}/content-type/${data.uid}/content-type-builder?branch=main`} target="_blank">Open <Icon icon="InternalLink"></Icon></a>)
+            }
+        },
+    ]
 
-  const [isRawConfigModalOpen, setRawConfigModalOpen] = useState<boolean>(false);
+    const appSdk = useAppSdk()
+    const stack = appSdk?.stack
 
-  const handleViewRawConfig = useCallback(() => {
-    setRawConfigModalOpen(true);
-  }, []);
+    const [contentTypes, setContentTypes] = useState<any[]>([])
+    const [itemStatusMap, setItemStatusMap] = useState<string[]>([])
+    const tableRef = useRef<HTMLDivElement | null>(null)
 
-  const handleCloseModal = useCallback(() => {
-    setRawConfigModalOpen(false);
-  }, []);
+    const fetchData = async () => {
+        try {
+            let itemStatusMapCopy = { ...itemStatusMap }
 
-  const sampleAppConfig = appConfig?.["sample_app_configuration"] || "";
-  const trimmedSampleAppConfig =
-    sampleAppConfig.length > 19 ? `${sampleAppConfig.substring(0, 19)}...` : sampleAppConfig;
+            let contentTypesObj: ContentTypesResponse = {}
+            let contentTypesWithCount: ContentTypesResponse = {}
+            if (stack && typeof stack.getContentTypes === "function") {
+                contentTypesObj = await stack.getContentTypes()
 
-  return (
-    <div className="layout-container">
-      <div className="ui-location">
-        <div className="ui-container">
-          <div className="logo-container">
-            <img src={Icon} alt="Logo" />
-            <p>{localeTexts.FullPage.title}</p>
-          </div>
-          <div className="config-container">
-            <div className="label-container">
-              <p className="label">Sample App Configuration</p>
-              <p className="info">(read only)</p>
+                setContentTypes(contentTypesObj.content_types ?? [])
+                if (contentTypesObj.content_types) {
+                    for (let index = 0; index < contentTypesObj.content_types.length; index++) {
+                        itemStatusMapCopy[index] = 'loading'
+                    }
+                }
+                setItemStatusMap(itemStatusMapCopy)
+            }
+            if (stack && contentTypesObj.content_types) {
+                console.log('hi')
+                contentTypesWithCount.content_types = await Promise.all(contentTypesObj.content_types.map(async (contentType) => {
+                    const count = await stack.ContentType(contentType.uid).Entry.Query().count()
+                    return { ...contentType, count: count.entries }
+                }))
+                if (contentTypesObj.content_types) {
+                    for (let index = 0; index < contentTypesObj.content_types.length; index++) {
+                        itemStatusMapCopy[index] = 'loaded'
+                    }
+                }
+                setItemStatusMap(itemStatusMapCopy)
+                console.log("Content Types with Count:", contentTypesWithCount)
+                setContentTypes(contentTypesWithCount.content_types ?? [])
+            }
+        } catch (error) {
+            console.log('fetchData -> error', error)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    return (
+        <div className="layout-container">
+            <div className="ui-location">
+                {contentTypes.length > 0 ?
+                    <InfiniteScrollTable data={contentTypes}
+                        columns={columns}
+                        uniqueKey={'uid'}
+                        fetchTableData={fetchData}
+                        // loading={loading}
+                        // totalCounts={totalCounts}
+                        loadMoreItems={fetchData}
+                        itemStatusMap={itemStatusMap}
+                        columnSelector={true}
+                        minBatchSizeToFetch={50}
+                        viewSelector={true}
+                        // getViewByValue={getViewByValue}
+                        initialSortBy={[{ id: 'title', desc: false }]}
+                        searchPlaceholder={'Search'}
+                        // canSearch={true}
+                        canRefresh={true}
+                        ref={tableRef}
+                    // initialSortByV2={sortByV2}
+                    // callBackForSortByV2={(sortBy) => {
+                    //     console.log('sorting here callback', sortBy)
+                    //     setSortByV2(sortBy)
+                    //     fetchData({ sortBy, skip: 0, limit: 30 })
+
+                    /> : <></>}
             </div>
-            <div className="input-wrapper">
-              <div className="input-container">
-                <p className="config-value">{trimmedSampleAppConfig}</p>
-                <img src={ReadOnly} alt="ReadOnlyLogo" />
-              </div>
-
-              <img src={JsonView} alt="Show-Json-CTA" className="show-json-cta" onClick={handleViewRawConfig} />
-              {isRawConfigModalOpen && appConfig && <ConfigModal config={appConfig} onClose={handleCloseModal} />}
-            </div>
-          </div>
-          <div className="location-description">
-            <p className="location-description-text">{parse(localeTexts.FullPage.body)}</p>
-            <a target="_blank" rel="noreferrer" href={localeTexts.FullPage.button.url}>
-              <span className="location-description-link">{localeTexts.FullPage.button.text}</span>
-            </a>
-          </div>
         </div>
-      </div>
-    </div>
-  );
-};
-export default FullPageExtension;
+    )
+}
+export default FullPageExtension
